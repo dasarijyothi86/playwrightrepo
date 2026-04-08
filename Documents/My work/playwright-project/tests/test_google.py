1b8364dd4aa72fe447f30dc93ae5016d18d7bc62
Jyothi D <dasari.jyothi@gmail.com>
2026-04-08 15:51:03 +0530

    trigger test

--- Documents/My work/playwright-project/tests/test_google.py
+++ Documents/My work/playwright-project/tests/test_google.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def test_google_title():
-    print("Trigger test")   # 👈 add here
+    print("Trigger test")   # 👈 add here 
 
     driver = webdriver.Chrome()
     driver.get("https://www.google.com")
